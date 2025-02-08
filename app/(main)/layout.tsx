356eb621e643e4fa6d977ff42f0189bd98e478bb
tushargirase <tushargirase1704@gmail.com>
2025-02-08 09:29:43 +0530

    initial commit

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from 'next';
+import Layout from '../../layout/layout';
+
+interface AppLayoutProps {
+    children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+    title: 'ABCL Corp',
+    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
+    robots: { index: false, follow: false },
+    viewport: { initialScale: 1, width: 'device-width' },
+    openGraph: {
+        type: 'website',
+        title: 'ABCL Corp',
+        url: '/',
+        description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
+        ttl: 604800
+    },
+    icons: {
+        icon: '/favicon.ico'
+    }
+};
+
+export default function AppLayout({ children }: AppLayoutProps) {
+    return <Layout>{children}</Layout>;
+}
